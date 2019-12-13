e2578f4bdd51f459f0b2b08504fbb3baae17c983
jinyt <jinyt@authine.com>
2019-12-13 16:13:55 +0800

    添加测试界面

--- src/components/baseList/base.js
+++ src/components/baseList/base.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Base extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 1
+    }
+  }
   render(){
     return(
     <div>
-      基础界面
+      基础界面=>{this.state.count}
     </div>
     );
   }
